84f96e343db139dc1d451223b7ddbd7b444abc6a
EagleJ0hn <juanby1928@gmail.com>
2026-08-02 14:04:46 -0700

    connected login to backend

---
+++ public/login.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const loginData = {
+        username: username,
+        password: password
+    };
+
+    try{
+        const response = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginData)
+        });
+
+        const result = await response.json();
+
+        alert(result.message);
+        
+    } catch (error) {
+        console.error(error);
+        alert("An error occurred while logging in.");
+    }
+});
