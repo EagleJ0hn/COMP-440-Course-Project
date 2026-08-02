f54a04ee9845a4212d30f28f75c1901bc5dc1ca2
EagleJ0hn <juanby1928@gmail.com>
2026-08-02 12:49:29 -0700

    implemented authroization and validation

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 
+const { registerUser, loginUser } = require("./server/auth");
+const { validateInput } = require("./server/validation");
+
 const express = require("express");
 const path = require("path");
 const pool = require("./db");
+const { register } = require("module");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
@@ -24,6 +29,64 @@ app.get("/db-test", async (req, res) => {
     } catch (error) {
         console.error(error);
         res.status(500).send("Could not connect to MySQL");
+    }
+});
+
+app.post("/api/register", async (req, res) => {
+    try{
+        // Validate the input data
+        const validation = validateInput(req.body);
+
+        if (!validation.valid) {
+            return res.status(400).json(validation);
+        }
+
+        // Register the user
+        const result = await registerUser(req.body);
+
+        if (!result.success){
+            return res.status(409).json(result);
+        }
+
+        res.status(201).json(result);
+    
+    } catch (error) {
+        console.error("Registration error:", error);
+
+        res.status(500).json({
+            success: false,
+            message: "An error occured during registration."
+        });
+    }
+})
+
+app.post("/api/login", async (req, res) => {
+    try {
+        const { username, password } = req.body;
+
+        // Make sure the login fields were provided
+        if (!username || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Username and password are required."
+            });
+    }
+
+    //Authenticate the user
+    const result = await loginUser(username, password);
+
+    if (!result.success) {
+        return res.status(401).json(result);
+    }
+
+    res.status(200).json(result);
+    } catch (error) {
+        console.error("Login error:", error);
+
+        res.status(500).json({
+            success: false,
+            message: "An error occurred during login."
+        });
     }
 });
 
